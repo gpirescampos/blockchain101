ca1617ecb13f3baa7136ccc9a91146e3248e8e71
Guilherme Campos <guilherme@Guilhermes-MacBook-Pro.local>
2017-10-05 19:20:33 +0100

    modified create transaction endpoint

--- blockchain.py
+++ blockchain.py
@@ -113,7 +113,18 @@ def mine():
   
 @app.route('/transactions/new', methods=['POST'])
 def new_transaction():
-    return "We'll add a new transaction"
+    values = request.get_json()
+
+    # Check that the required fields are in the POST'ed data
+    required = ['sender', 'recipient', 'amount']
+    if not all(k in values for k in required):
+        return 'Missing values', 400
+
+    # Create a new Transaction
+    index = blockchain.new_transaction(values['sender'], values['recipient'], values['amount'])
+
+    response = {'message': f'Transaction will be added to Block {index}'}
+    return jsonify(response), 201
 
 @app.route('/chain', methods=['GET'])
 def full_chain():
